a7773ef52d769cb03d584d07945d5172c645e4c9
mashiro <mail@mashiro.org>
2014-10-02 22:19:39 +0900

    symbolize

--- spec/enver/loader_spec.rb
+++ spec/enver/loader_spec.rb
@@ -32,34 +32,35 @@ RSpec.describe Enver::Loader do
   end
 
   describe '#string' do
-    it { expect(loader.string('v', 'STRING1')).to eq('') }
-    it { expect(loader.string('v', 'STRING2')).to eq('test') }
+    it { expect(loader.string(:v, 'STRING1')).to eq('') }
+    it { expect(loader.string(:v, 'STRING2')).to eq('test') }
   end
 
   describe '#integer' do
-    it { expect{loader.integer('v', 'INTEGER1')}.to raise_error }
-    it { expect(loader.integer('v', 'INTEGER2')).to eq(1) }
-    it { expect{loader.integer('v', 'INTEGER3')}.to raise_error }
-    it { expect{loader.integer('v', 'INTEGER4')}.to raise_error }
+    it { expect{loader.integer(:v, 'INTEGER1')}.to raise_error }
+    it { expect(loader.integer(:v, 'INTEGER2')).to eq(1) }
+    it { expect{loader.integer(:v, 'INTEGER3')}.to raise_error }
+    it { expect{loader.integer(:v, 'INTEGER4')}.to raise_error }
   end
 
   describe '#boolean' do
-    it { expect(loader.boolean('v', 'BOOLEAN1')).to eq(false) }
-    it { expect(loader.boolean('v', 'BOOLEAN2')).to eq(true) }
-    it { expect(loader.boolean('v', 'BOOLEAN3')).to eq(false) }
-    it { expect(loader.boolean('v', 'BOOLEAN4')).to eq(true) }
-    it { expect(loader.boolean('v', 'BOOLEAN5')).to eq(false) }
-    it { expect(loader.boolean('v', 'BOOLEAN6')).to eq(true) }
-    it { expect(loader.boolean('v', 'BOOLEAN7')).to eq(false) }
-    it { expect(loader.boolean('v', 'BOOLEAN8')).to eq(true) }
-    it { expect(loader.boolean('v', 'BOOLEAN9')).to eq(false) }
-    it { expect(loader.boolean('v', 'BOOLEAN10')).to eq(false) }
-    it { expect(loader.boolean('v', 'BOOLEAN10', true_values: %w(はい))).to eq(true) }
+    it { expect(loader.boolean(:v, 'BOOLEAN1')).to eq(false) }
+    it { expect(loader.boolean(:v, 'BOOLEAN2')).to eq(true) }
+    it { expect(loader.boolean(:v, 'BOOLEAN3')).to eq(false) }
+    it { expect(loader.boolean(:v, 'BOOLEAN4')).to eq(true) }
+    it { expect(loader.boolean(:v, 'BOOLEAN5')).to eq(false) }
+    it { expect(loader.boolean(:v, 'BOOLEAN6')).to eq(true) }
+    it { expect(loader.boolean(:v, 'BOOLEAN7')).to eq(false) }
+    it { expect(loader.boolean(:v, 'BOOLEAN8')).to eq(true) }
+    it { expect(loader.boolean(:v, 'BOOLEAN9')).to eq(false) }
+    it { expect(loader.boolean(:v, 'BOOLEAN10')).to eq(false) }
+    it { expect(loader.boolean(:v, 'BOOLEAN10', true_values: %w(はい))).to eq(true) }
   end
 
   describe '#array' do
-    it { expect(loader.array('v', 'ARRAY1')).to eq(['foo', 'bar', 'buzz']) }
-    it { expect(loader.array('v', 'ARRAY2', pattern: ':')).to eq(['foo', 'bar', 'buzz']) }
-    it { expect(loader.array('v', 'ARRAY1', limit: 2)).to eq(['foo', 'bar,buzz']) }
+    it { expect(loader.array(:v, 'ARRAY1')).to eq(['foo', 'bar', 'buzz']) }
+    it { expect(loader.array(:v, 'ARRAY2', pattern: ':')).to eq(['foo', 'bar', 'buzz']) }
+    it { expect(loader.array(:v, 'ARRAY1', limit: 2)).to eq(['foo', 'bar,buzz']) }
+  end
   end
 end
